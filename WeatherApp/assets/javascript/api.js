a4add98713407706be9e5e2f51411494cd060950
Rahul Siddhardh <siddhardh6002@gmail.com>
2023-05-17 14:55:41 +0530

    Implemented Search Function

---
+++ WeatherApp/assets/javascript/api.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const api_key = "23982ac12791ffe3d0d82b1741140f61";
+
+/**
+ * Fetch data from server
+ * @param {string} URL API url
+ * @param {Function} callback callback
+ */
+export const fetchData = function (URL, callback) {
+  fetch(`${URL}&appid=${api_key}`)
+    .then((res) => res.json())
+    .then((data) => callback(data));
+};
+
+export const url = {
+  currentWeather(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`;
+  },
+  forecast(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`;
+  },
+  airPollution(lat, lon) {
+    return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`;
+  },
+  geocoding_rev(lat, lon) {
+    retrun`http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=6`;
+  },
+  /**
+   * @param {string} query Search query eg: Hyderbad,IN or Chennai,IN
+   */
+  geocoding(query) {
+    return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`;
+  },
+};
